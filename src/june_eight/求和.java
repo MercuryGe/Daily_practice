b2dafe932e092efab3fcab6dc8764babdba941fd
MercuryGe <mercuryge@163.com>
2021-06-08 16:55:53 +0800

    六月八日

--- src/june_eight/求和.java
+++ src/june_eight/求和.java
@@ -3,6 +3,7 @@ package june_eight;
 import java.util.Scanner;
 
 public class 求和 {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         while(scan.hasNext()){
